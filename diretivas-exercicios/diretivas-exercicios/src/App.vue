a25c8f8dd50f34acd217e58c78380fd681c2febb
yankayohana <yanka.neves@ccc.ufcg.edu.br>
2021-06-17 16:30:43 -0300

    criando diretivas personalizadas

---
+++ diretivas-exercicios/diretivas-exercicios/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+	<div id="app">
+		<h1>Diretivas</h1>
+		<hr/>
+		<p v-text=" 'estou usando uma diretiva de texto' "></p> 
+		<p v-html="'usando uma <strong>v-html</strong>'"></p>
+		<hr/>
+		<p v-destaque="'yellow'">Usando uma diretiva personalizada</p>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 2.5rem;
+}
+</style>
