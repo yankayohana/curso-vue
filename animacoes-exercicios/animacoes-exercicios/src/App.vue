30da210f4a48a16fd91bf4b3d0d728ac81053b87
yankayohana <yanka.neves@ccc.ufcg.edu.br>
2021-06-20 20:17:40 -0300

    animacoes

---
+++ animacoes-exercicios/animacoes-exercicios/src/App.vue
@@ -0,0 +1,93 @@
+<template>
+	<div id="app" class="container-fluid">
+		<h1>Animações</h1>
+		<b-button variant="primary" class="mb-4" @click="clicado = !clicado">clique aqui</b-button>
+
+		<transition name="fade" mode="out-in">
+			<b-alert variant="info" show v-if="clicado" key="info"> {{ msg }} </b-alert>	
+			<b-alert variant="warning" show v-else key="warn"> {{ msg }} </b-alert>	
+		</transition>
+
+
+		<transition name="slide" appear="">
+			<b-alert variant="info" show v-show="clicado"> {{ msg }} </b-alert>		
+		</transition>
+		
+	</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "eu fui clicado viu",
+      clicado: true,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 1.5rem;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-to,  /** aqui não é obrigatio colocar esse, pois já 1 por default **/
+.fade-leave {
+  opacity: 1;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 2s;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateY(40px);
+  }
+  to {
+    transform: translateY(0);
+  }
+}
+
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+  }
+  to {
+    transform: translateY(40px);
+  }
+}
+
+.slide-enter,
+.slide-leave-to {
+  opacity: 0;
+}
+
+.slide-enter-to,
+.slide-leave {
+  opacity: 1;
+}
+
+.slide-enter-active {
+  animation: slide-in 2s ease;
+  transition: opacity 2s;
+}
+
+.slide-leave-active {
+  animation: slide-out 2s ease;
+  transition: opacity 2s;
+}
+</style>
---
+++ filtros-mixins-exercicios/filtros-mixins-exercicios/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+	<div id="app">
+		<h1>Filtros & Mixins</h1>
+		<hr>
+		<h3> {{ cpf | cpf | inverter}} </h3>
+	</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cpf: "09167898790",
+    };
+  },
+
+  filters: {
+    cpf(valor) {
+      const arr = `${valor}`.split("");
+      arr.splice(3, 0, ".");
+      arr.splice(7, 0, ".");
+      arr.splice(11, 0, "-");
+      return arr.join("");
+    },
+  },
+};
+</script>
+	
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 2.5rem;
+}
+</style>
